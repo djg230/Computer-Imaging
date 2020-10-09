88fc3915598db7141b1bfbc043d7a9b40b5c3a0d
djg230 <72278809+djg230@users.noreply.github.com>
2020-10-09 03:21:05 -0400

    Add files via upload

---
+++ Color_Counter.java
@@ -0,0 +1,30 @@
+public void run(ImageProcessor ip) {
+		int red = 0;
+		int blue = 0;
+		int green = 0;
+		int white = 0;
+		int black = 0;
+		int[] pixels = (int[])ip.getPixels();
+		for (int i=0; i<pixels.length; i++)
+			if(pixels[i] = int((0xff0000)>>16){
+				red++;
+			}
+			if(pixels[i] = int((0x00ff00)>>8){
+				green++;
+			}
+			if(pixels[i] = int((0x0000ff)){
+				blue++;
+			}
+			if(pixels[i] = int((0xff0000)>>16){
+				red++;
+			}
+			if(pixels[i] = 0){
+				black++;
+			}
+			if(pixels[i] = int((0xffffff)){
+				red++;
+			}
+			
+			
+		}
+	}
