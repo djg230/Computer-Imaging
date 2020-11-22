539163ca4beb018bc17b83ac93b1616e5c891432
djg230 <72278809+djg230@users.noreply.github.com>
2020-11-22 16:13:50 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,87 +1,130 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from scipy.spatial.distance import cdist
-from skimage.measure import label, regionprops, moments, moments_central,
-moments_normalized, moments_hu
+from skimage.measure import label, regionprops, moments, moments_central, moments_normalized, moments_hu
 from skimage import io, exposure
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import pickle
+import os
+import statistics
 
-
-directory =
-for photo in os.listdir(directory)
-    count = 0
-    pname = photo.replace('.bmg', "")
-    img = io.imread(photo);
-
-    print (img.shape)
-
+directory = '/Users/davidgiansanti/Desktop/images/'
+database = []
+Features=[]
+for photo in os.listdir(directory):
+    print(photo)
+    count = 0 
+    pname = photo.replace('.bmp', "")
+    # photo = "/Users/davidgiansanti/Desktop/images/w.bmp"
+    img = io.imread(directory + photo);
+    #print(img.shape)
+    
     io.imshow(img)
     plt.title('Original Image')
     io.show()
-
+    
     hist = exposure.histogram(img)
     plt.bar(hist[1], hist[0])
     plt.title('Histogram')
     plt.show()
-
+    
     th = 200
     img_binary = (img < th).astype(np.double)
     io.imshow(img_binary)
     plt.title('Binary Image')
     io.show();
-
-    img_label = label(img-binary, background = 0)
-
+    
+    img_label = label(img_binary, background = 0)
+    
     io.imshow(img_label)
     plt.title('Labeled image')
     io.show()
-
-    print(np.amax(img_label))
-
+    
+    #print(np.amax(img_label))
+    
     regions = regionprops(img_label)
     io.imshow(img_binary)
     ax = plt.gca()
-
-    Features=[]
-    Char =[][]
-    Char[1][count] = pname
+    
+    count = 0
+    Char = []
+    temp = []
     for props in regions:
+        cnr = 0
         minr,minc, maxr,maxc = props.bbox
         roi = img_binary[minr:maxr, minc:maxc]
-        m= moments(roi)
+        m = moments(roi)
         cc = m[0, 1] / m[0, 0]
         cr = m[1, 0] / m[0, 0]
         mu = moments_central(roi, center=(cr,cc))
         nu = moments_normalized(mu)
         hu = moments_hu(nu)
-        features.append(hu)
+        Features.append(hu)
+        Char.append(hu)
         ax.add_patch(Rectangle((minc, minr), maxc - minc, maxr - minr,
         fill = False, edgecolor = 'red', linewidth = 1))
+        #Char.append(pname)
     ax.set_title('Bounding Boxes')
     io.show()
+   # Char.append(Features)
+    for ch in Char:
+        database.append(pname)
+    #database.append(Features)
+             
+    
 
+print(database[3])
+    
+cnr =1
+temp = []
+for f in Features:
+    temp.append(cnr)
+    cnr  =cnr+1 
+print(len(temp))
+   
+
+for x in Features:
+    #for l in x:
+    mean = statistics.mean(x)
+    sdev = statistics.stdev(x)
+
+for c in Features:
+    for r in c:
+       r = r - mean
+       r = r / sdev
+    
 
 D = cdist(Features, Features)
 io.imshow(D)
 plt.title('Distance Matrix')
 io.show()
 
-D_index = np.argsort(dist, axis = 1)
+dist = []
+for c in D:
+    for r in c:
+        dist = D
 
-cntr = 0
-cls = []
+for clm in dist:
+    D_index = np.argsort(dist, axis = 1)
+    
+    
+    
+Ytrue = []
+Ypred = []
+print(len(database))
+print(len(Features))
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-for clmn in D_index
-    cls = class[cntr]
-    Ypred = column(D_index, 1)
-    Ytrue = cls
+Ypred = column(D_index, 1)
+Ytrue = temp
 
-confusion_matrix(Ytrue, Ypred)
 
+
+confM = confusion_matrix(Ytrue, Ypred)
 io.imshow(confM)
 plt.title('Confusion Matrix')
 io.show()
+
+    
