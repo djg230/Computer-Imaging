52d50cdbf6fac8de47b42517e50cbd741a157aff
djg230 <72278809+djg230@users.noreply.github.com>
2020-11-28 23:31:23 -0500

    Add files via upload

--- RunMyOCR.py
+++ RunMyOCR.py
@@ -1,14 +1,16 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from scipy.spatial.distance import cdist
-from skimage.measure import label, regionprops, moments, moments_central, moments_normalized, moments_hu
+from skimage.measure import label, regionprops, moments, moments_central,inertia_tensor, moments_normalized, moments_hu, find_contours, profile_line
 from skimage import io, exposure
 import matplotlib.pyplot as plt
+from skimage.morphology import binary_closing
 from matplotlib.patches import Rectangle
 import pickle
 import os
 import statistics
 import sys
+from skimage.filters import threshold_mean, threshold_minimum, threshold_otsu
 
 pkl_file = open('test_gt_py3.pkl', 'rb')
 mydict = pickle.load(pkl_file)
@@ -22,6 +24,8 @@ database = []
 Features=[]
 for photo in os.listdir(directory):
     if(photo == 'test.bmp'):
+        continue
+    if(photo == '.DS_Store'):
         continue
     print(photo)
     count = 0 
@@ -39,8 +43,9 @@ for photo in os.listdir(directory):
     plt.title('Histogram')
     plt.show()
     
-    th = 200
+    th = threshold_otsu(img)
     img_binary = (img < th).astype(np.double)
+    img_binary = binary_closing(img_binary)
     io.imshow(img_binary)
     plt.title('Binary Image')
     io.show();
@@ -109,8 +114,9 @@ plt.bar(hist[1], hist[0])
 plt.title('Histogram')
 plt.show()
  
-th = 200
+th = threshold_otsu(img)
 img_binary = (img < th).astype(np.double)
+img_binary = binary_closing(img_binary)
 io.imshow(img_binary)
 plt.title('Binary Image')
 io.show();
@@ -168,24 +174,64 @@ for clm in dist:
     
 def column(matrix, i):
     return [row[i] for row in matrix]
-temp = []
-Ypred = column(D_index, 1)
+nbr = 0
+neighbor = []
+neighbors = []
+countr = 0
+D_index = np.argsort(D, axis = 1)
+for ind in D_index:
+    for i in ind:
+        if nbr < 10:
+            neighbor.append(i)
+        nbr = nbr + 1
+    neighbor = []
+    nbr = 0
+    neighbors.append(neighbor)
+    
+similar = 0
+oldsim = 0
+voted = []
+for n in neighbors:
+    for nb in n:
+        nb = database[nb]
+        for x in n:
+            x = database[x]
+            if nb == x:
+                similar = similar + 1
+        if similar > oldsim:
+            vote = nb
+        oldsim = similar
+        similar = 0
+    voted.append(vote)
+            
+
+Ypred = voted
+for v in Ypred:
+    print(v)
 cnr = 1
-for f in testF:
-    temp.append(cnr)
-    cnr  =cnr+1 
-Ytrue = classes
 
-c1 = 0
-for l in Ypred:
-   l = database[l]
-  
-   Ypred[c1] = l
-   c1 = c1+1
- 
-Ytrue = []
-c0 = 0
 
-for tr in locations:
-    print(tr)
+c= 0
+
+location = column(locations,1)
+
+l1 = column(locations, 0)
+l2 = column(locations, 1)
+
+co = 0
+for l in classes:
+    if Ypred[c] == l:
+        co = co+1
+    c = c+1
+
+
+c  = 1
+c3 = 0
+
+
+
+print("Letters Recognized: " + str(co))
+print("Recognition Rate: " + str((co/len(Ypred)) *100) + "%")
+
+
             
